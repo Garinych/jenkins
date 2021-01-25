b714795c1ef99b27339e2553b84a66ccb729fd45
Dmitriy Sandul <dmitriy.sandul@ourbox.org>
2021-01-25 10:57:14 +0200

    add pipelineJob for theme-park-rides project

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -13,3 +13,18 @@ pipeline {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/Garinych/theme-park-rides.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
